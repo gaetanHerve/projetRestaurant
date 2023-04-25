73ecf7a580b153d87428a8e3ed5b8b24e25a5119
Laure Le Nouail <laure.lenouail@gmail.com>
2023-04-25 13:57:54 +0200

    ajout lien nav bar

--- restaurantWebApp/WebContent/WEB-INF/authentification.jsp
+++ restaurantWebApp/WebContent/WEB-INF/authentification.jsp
@@ -17,7 +17,7 @@
 		src="images/logo.jpg"> <nav
 		class="navbar navbar-expand-lg bg-success">
 	<div class="container-fluid">
-		<a class="navbar-brand" href="accueil.html">Eat & Love</a>
+		<a class="navbar-brand" href="./accueil.jsp">Eat & Love</a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
 			data-bs-target="#navbarSupportedContent"
 			aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -27,11 +27,11 @@
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 				<li class="nav-item"><a class="nav-link active"
-					aria-current="page" href="carte.html">Notre carte</a></li>
-				<li class="nav-item"><a class="nav-link" href="#">Login</a></li>
-				<li class="nav-item"><a class="nav-link" href="#">Inscription</a>
+					aria-current="page" href="carte">Notre carte</a></li>
+				<li class="nav-item"><a class="nav-link" href="authentification">Login</a></li>
+				<li class="nav-item"><a class="nav-link" href="inscription">Inscription</a>
 				</li>
-				<li class="nav-item"><a class="nav-link" href="#">Mon Menu</a>
+				<li class="nav-item"><a class="nav-link" href="panier">Mon Menu</a>
 				</li>
 			</ul>
 		</div>
---
+++ restaurantWebApp/src/srv/ServletRouteInscription.java
@@ -0,0 +1,41 @@
+package srv;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletInscription
+ */
+@WebServlet("/inscription")
+public class ServletRouteInscription extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletRouteInscription() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("WEB-INF/inscription.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
